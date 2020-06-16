b90a51a16f7d1ae191a19289b2105ffb33c5e0b1
Mohamed Ahmed <41699093+I3iGM0@users.noreply.github.com>
2020-06-16 21:38:10 +0100

    Creaye a Movie Card Component
    Split original search movie component in to 2. Movice card is it's own component which takes in a movie as input. (props)

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 //import { render } from '@testing-library/react';
 
+//import movieCard component
+import MovieCard from './movieCard.js';
 
 export default function SearchMovies(){
     
@@ -9,7 +11,6 @@ export default function SearchMovies(){
 
     //State for movies when query updates
     const [movies, setMovies] = useState([]);
-
 
     //API movie request
     const searchMovies = async (e) => {
@@ -42,19 +43,8 @@ export default function SearchMovies(){
 
         <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                            />
-                        <div className="card--content">
-                        <h3 className="card--title">{movie.title}</h3>
-                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                        <p><small>RATING: {movie.vote_average}</small></p>
-                        <p className="card--desc">{movie.overview}</p>
-                        </div>
-
-                    </div>
+                    //Pass the movie propery to the MovieCard component
+                   <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div> 
         </>
