fdc70d666aac48f15c0d31948fd1efaff114a33f
Mohamed Ahmed <41699093+I3iGM0@users.noreply.github.com>
2020-06-16 20:44:04 +0100

    Create SearchMovie function
    Makes an Async API call to the movie db and retrieves results from request.

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
+
 
 export default function SearchMovies(){
+    
+    //API movie request
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+        
+        const query = "Jurassic Park";
+        
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=b097657f6fb5d9c1066fa666bf592267&language=en-US&query=${query}&page=1&include_adult=true`;
+        
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
                 placeholder="i.e. Jurassic Park"/>
