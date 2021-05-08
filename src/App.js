0da8127786112cf8ddc2a9f55b58506773999b9e
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2021-05-08 17:14:40 +0100

    logo github

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-//import logo from './logo.svg';
+import GitHubIcon from "@material-ui/icons/GitHub";
 import './App.css';
 
 // Import components
 import SearchMovies from './searchMovie'
 function App() {
   return (
-    <div className="container">
-    <h1 className = "title">Movie Search!</h1>
+    <div>
+      <div className= "github-link"> 
+        <a href="https://github.com/Mo-Ali98/movie-search-app">
+        <GitHubIcon style={{ fontSize: 22 }}></GitHubIcon>
+        </a>
+</div>
+
+      <div className = "container">
+      <h1 className = "title">Movie Search!</h1>
         <SearchMovies/>
+      </div>
+
   </div>
   );
 }
